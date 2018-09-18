03133e8a8f4aa851eb98761c2db7a99f8c2f7f79
Manny <egalli64@gmail.com>
2018-09-18 20:26:04 +0200

    try-with-resource for ApplicationContext

--- chapter02/hello-world/src/test/java/com/apress/prospring5/ch2/TestHelloWorldSpringAnnotated.java
+++ chapter02/hello-world/src/test/java/com/apress/prospring5/ch2/TestHelloWorldSpringAnnotated.java
@@ -3,7 +3,6 @@ package com.apress.prospring5.ch2;
 import com.apress.prospring5.ch2.annotated.HelloWorldConfiguration;
 import com.apress.prospring5.ch2.decoupled.MessageRenderer;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.Assert.assertNotNull;
@@ -13,11 +12,18 @@ import static org.junit.Assert.assertNotNull;
  */
 public class TestHelloWorldSpringAnnotated {
 
-	@Test
-	public void testHello() {
-		ApplicationContext ctx = new AnnotationConfigApplicationContext
-				(HelloWorldConfiguration.class);
-		MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
-		assertNotNull(mr);
-	}
+    @Test
+    public void testHello() {
+        /**
+         * Using try-with-resource
+         * 
+         * @author manny
+         * @see http://thisthread.blogspot.com/2018/09/how-to-close-spring-applicationcontext.html
+         */
+        try (AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
+                HelloWorldConfiguration.class)) {
+            MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
+            assertNotNull(mr);
+        }
+    }
 }
--- chapter02/hello-world/src/test/java/com/apress/prospring5/ch2/TestHelloWorldSpringDI.java
+++ chapter02/hello-world/src/test/java/com/apress/prospring5/ch2/TestHelloWorldSpringDI.java
@@ -2,7 +2,6 @@ package com.apress.prospring5.ch2;
 
 import com.apress.prospring5.ch2.decoupled.MessageRenderer;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import static org.junit.Assert.assertNotNull;
@@ -12,11 +11,17 @@ import static org.junit.Assert.assertNotNull;
  */
 public class TestHelloWorldSpringDI {
 
-	@Test
-	public void testHello() {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext
-				("spring/app-context.xml");
-		MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
-		assertNotNull(mr);
-	}
+    @Test
+    public void testHello() {
+        /**
+         * Using try-with-resource
+         * 
+         * @author manny
+         * @see http://thisthread.blogspot.com/2018/09/how-to-close-spring-applicationcontext.html
+         */
+        try (ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring/app-context.xml")) {
+            MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
+            assertNotNull(mr);
+        }
+    }
 }
