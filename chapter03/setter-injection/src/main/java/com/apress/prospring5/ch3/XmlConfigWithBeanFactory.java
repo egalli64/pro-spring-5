7b8563e0779543ac9e5384583072e2c88e1510e5
Manny <egalli64@gmail.com>
2018-09-20 12:46:06 +0200

    eg: NoSuchBeanDefinitionException example added

--- chapter03/setter-injection/src/main/java/com/apress/prospring5/ch3/XmlConfigWithBeanFactory.java
+++ chapter03/setter-injection/src/main/java/com/apress/prospring5/ch3/XmlConfigWithBeanFactory.java
@@ -1,5 +1,6 @@
 package com.apress.prospring5.ch3;
 
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
@@ -10,6 +11,13 @@ public class XmlConfigWithBeanFactory {
         DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
         XmlBeanDefinitionReader rdr = new XmlBeanDefinitionReader(factory);
         rdr.loadBeanDefinitions(new ClassPathResource("spring/xml-bean-factory-config.xml"));
+
+        try {
+            factory.getBean("There's not a bean with this name");
+        } catch (NoSuchBeanDefinitionException nsb) {
+            System.out.println("Be careful to input the right bean name!");
+        }
+
         Oracle oracle = (Oracle) factory.getBean("oracle");
         System.out.println(oracle.defineMeaningOfLife());
     }
